21ca1250a4a9458eeb59d5f223d1955288c41459
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-09-10 20:54:57 +0900

    @기호는 지정어와 함께 사용할 때는 지정어와 구분되지만 일반 명칭과 함께 사용되면 동일한 명칭으로 인식한다. i와 @i는 같은 명칭이어서 함께 선언할 수 없다.

---
+++ Code/IdentifierApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class IdentifierApp
+{
+    public static void Main()
+    {
+        int MyVar = 1, MyVar2 = 2;
+        int @int = 10, 변수 = 20;
+
+        Console.WriteLine("MyVar = " + MyVar + ", MyVar2 = " + MyVar2);
+        Console.WriteLine("@int = " + @int+ ", 변수 = " + 변수);
+    }
+}

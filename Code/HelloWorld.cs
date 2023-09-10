3223c80d2c008c9edb452417adad131fdc1bd606
손봉균 <124877603+sbg0629@users.noreply.github.com>
2023-09-10 20:23:05 +0900

    Code풀더에 C# 코드를 모두 넣을 것.

--- Code/HelloWorld.cs
+++ Code/HelloWorld.cs
@@ -5,5 +5,8 @@ class HelloWorld
     public static void Main()
     {
         Console.WriteLine("Hello World!");
+        //Console.WriteLine() = 출력 메소드.
+        //using System을 안 적으면 
+        //System.Console.WriteLine()로 명시 해야함.
     }
 }
